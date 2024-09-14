be15fef403efc60b94684df93f910a4b3732f5ee
SmallTeddy <807996041@qq.com>
2024-09-14 13:33:47 +0800

    Updates

---
+++ assets/basic_demo-509392b2.js
@@ -0,0 +1,10 @@
+const n=`<template>
+  <an-space>
+    <an-icon name="maximize" />
+    <an-icon name="phone" />
+    <an-icon name="trash" />
+    <an-icon name="announcement" />
+    <an-icon name="annotation-warning" />
+  </an-space>
+</template>
+`;export{n as default};
