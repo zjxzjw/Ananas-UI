474c7d1104e14f1d1c3fd767b500022a1b0d8ca4
SmallTeddy <807996041@qq.com>
2024-09-20 15:38:52 +0800

    Updates

---
+++ assets/basic_demo-b4d8a160.js
@@ -0,0 +1,11 @@
+const e=`<template>
+  <an-image :images="images"/>
+</template>
+
+<script setup>
+const images = ref([
+  'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
+  'https://img2.baidu.com/it/u=6343079,3944591376&fm=253&fmt=auto&app=138&f=JPEG?w=800&h'
+])
+<\/script>
+`;export{e as default};
