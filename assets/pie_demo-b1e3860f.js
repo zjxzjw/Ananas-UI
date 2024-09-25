59bbb9c3cc0658e2022adb41743c42fa743df56f
1500371773@qq.com <1500371773@qq.com>
2024-09-25 19:04:26 +0800

    Updates

---
+++ assets/pie_demo-b1e3860f.js
@@ -0,0 +1,25 @@
+const a=`<template>
+    <an-chart
+        :chart-data="barChartData"
+        chart-type="pie"
+    />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      barChartData: [
+        { name: 'Mon', value: 120 },
+        { name: 'Tue', value: 200 },
+        { name: 'Wed', value: 150 },
+        { name: 'Thu', value: 80 },
+        { name: 'Fri', value: 70 },
+        { name: 'Sat', value: 110 },
+        { name: 'Sun', value: 130 }
+      ]
+    };
+  }
+};
+<\/script>
+`;export{a as default};
