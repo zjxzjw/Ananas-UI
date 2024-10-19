2430915314be693b3111d4962fc6f52ac07d4661
1500371773@qq.com <1500371773@qq.com>
2024-10-19 12:11:50 +0800

    Updates

---
+++ assets/basic_demo-706363cd.js
@@ -0,0 +1,6 @@
+const n=`<template>
+  <an-number-animation :num="2648609927" :duration="1"/>
+</template>
+<script setup lang="ts">
+<\/script>
+`;export{n as default};
---
+++ assets/composite_demo-2c16a1dc.js
@@ -0,0 +1,49 @@
+const n=`<template>
+  <div>
+    <an-number-animation :num="num" :size="size" :duration="duration" />
+  </div>
+  <an-space :inline="true">
+    <an-button type="primary" @click="increaseNum">num + 10000</an-button>
+    <an-button type="primary" @click="reduceNum">num - 10000</an-button>
+    <an-button type="primary" @click="increaseSize">size + 10px</an-button>
+    <an-button type="primary" @click="reduceSize">size - 10px</an-button>
+    <an-button type="primary" @click="increaseDuration">duration + 1</an-button>
+    <an-button type="primary" @click="reduceDuration">duration - 1</an-button>
+  </an-space>
+</template>
+<script setup lang="ts">
+const num: Ref<number> = ref(12133)
+const size: Ref<number | string> = ref(24)
+const duration: Ref<number> = ref(1)
+
+const increaseNum = () => {
+  num.value += 10000
+}
+
+const reduceNum = () => {
+  num.value -= 10000
+}
+
+const increaseSize = () => {
+  (size.value as number) += 10
+}
+
+const reduceSize = () => {
+  if((size.value as number) <= 10) {
+    return
+  }
+  (size.value as number) -= 10
+}
+
+const increaseDuration = () => {
+  duration.value ++
+}
+
+const reduceDuration = () => {
+  if(duration.value <= 0) {
+    return
+  }
+  duration.value --
+}
+<\/script>
+`;export{n as default};
